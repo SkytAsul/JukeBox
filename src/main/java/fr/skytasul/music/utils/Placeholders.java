796588ffa8b246c4fb4f75bd96f2cf09e9f81fea
SkytAsul <skytasul@gmail.com>
2020-07-30 13:49:34 +0200

    1.18.2 - PlaceholderAPI fix

--- src/main/java/fr/skytasul/music/utils/Placeholders.java
+++ src/main/java/fr/skytasul/music/utils/Placeholders.java
@@ -1,49 +1,77 @@
 package fr.skytasul.music.utils;
 
-import org.bukkit.entity.Player;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.OfflinePlayer;
 
 import fr.skytasul.music.JukeBox;
 import fr.skytasul.music.PlayerData;
-import me.clip.placeholderapi.PlaceholderAPI;
-import me.clip.placeholderapi.PlaceholderHook;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 
-public class Placeholders {
+public class Placeholders extends PlaceholderExpansion {
 
 	public static void registerPlaceholders(){
-		if (PlaceholderAPI.unregisterPlaceholderHook("jukebox")) JukeBox.getInstance().getLogger().warning("Previous PlaceholderHook with identifier \"jukebox\" unregistered");
-		PlaceholderAPI.registerPlaceholderHook("jukebox", new PlaceholderHook() {
-			@Override
-			public String onPlaceholderRequest(Player p, String params){
-				PlayerData pdata = PlayerData.players.get(p.getUniqueId());
-				if (pdata == null) return "§c§lunknown player data";
-				if (params.startsWith("playeroptions_")){
-					switch (params.substring(params.indexOf("_") + 1)){
-					case "volume":
-						return pdata.getVolume() + "%";
-					case "shuffle":
-						return pdata.isShuffle() ? Lang.ENABLED : Lang.DISABLED;
-					case "join":
-						return pdata.hasJoinMusic() ? Lang.ENABLED : Lang.DISABLED;
-					case "particles":
-						return pdata.hasParticles() ? Lang.ENABLED : Lang.DISABLED;
-					case "loop":
-						return pdata.isRepeatEnabled() ? Lang.ENABLED : Lang.DISABLED;
-					default:
-						return "§c§lunknown option";
-					}
-				}else if (params.equals("active")){
-					if (pdata.songPlayer == null){
-						if (pdata.getPlaylistType() == Playlists.RADIO) return JukeBox.getSongName(JukeBox.radio.getSong());
-						return Lang.NONE;
-					}
-					return JukeBox.getSongName(pdata.songPlayer.getSong());
-				}else if (params.equals("playlist")){
-					return pdata.getPlaylistType().name;
-				}
-				return null;
-			}
-		});
+		new Placeholders().register();
 		JukeBox.getInstance().getLogger().info("Placeholders registered");
+	}
+	
+	private Placeholders() {}
+	
+	@Override
+	public String getAuthor() {
+		return JukeBox.getInstance().getDescription().getAuthors().toString();
+	}
+	
+	@Override
+	public String getIdentifier() {
+		return "jukebox";
+	}
+	
+	@Override
+	public String getVersion() {
+		return JukeBox.getInstance().getDescription().getVersion();
+	}
+	
+	@Override
+	public boolean canRegister() {
+		return true;
+	}
+	
+	@Override
+	public List<String> getPlaceholders() {
+		return Arrays.asList("playeroptions_volume", "playeroptions_shuffle", "playeroptions_join", "playeroptions_particles", "playeroptions_loop", "active", "playlist");
+	}
+	
+	@Override
+	public String onRequest(OfflinePlayer p, String params) {
+		PlayerData pdata = PlayerData.players.get(p.getUniqueId());
+		if (pdata == null) return "§c§lunknown player data";
+		if (params.startsWith("playeroptions_")) {
+			switch (params.substring(params.indexOf("_") + 1)) {
+			case "volume":
+				return pdata.getVolume() + "%";
+			case "shuffle":
+				return pdata.isShuffle() ? Lang.ENABLED : Lang.DISABLED;
+			case "join":
+				return pdata.hasJoinMusic() ? Lang.ENABLED : Lang.DISABLED;
+			case "particles":
+				return pdata.hasParticles() ? Lang.ENABLED : Lang.DISABLED;
+			case "loop":
+				return pdata.isRepeatEnabled() ? Lang.ENABLED : Lang.DISABLED;
+			default:
+				return "§c§lunknown option";
+			}
+		}else if (params.equals("active")) {
+			if (pdata.songPlayer == null) {
+				if (pdata.getPlaylistType() == Playlists.RADIO) return JukeBox.getSongName(JukeBox.radio.getSong());
+				return Lang.NONE;
+			}
+			return JukeBox.getSongName(pdata.songPlayer.getSong());
+		}else if (params.equals("playlist")) {
+			return pdata.getPlaylistType().name;
+		}
+		return null;
 	}
 	
 }
