4d31fc964b97bff95f172cea3a52dc49a2d37a08
SkytAsul <skytasul@gmail.com>
2023-10-09 20:30:34 +0200

    Update to 1.20.2

--- src/main/java/fr/skytasul/music/JukeBoxInventory.java
+++ src/main/java/fr/skytasul/music/JukeBoxInventory.java
@@ -1,14 +1,9 @@
 package fr.skytasul.music;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -21,12 +16,10 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import com.mojang.authlib.properties.PropertyMap;
 import com.xxmicloxx.NoteBlockAPI.model.Song;
-
 import fr.skytasul.music.utils.Lang;
 import fr.skytasul.music.utils.Playlists;
 
@@ -35,9 +28,9 @@ import fr.skytasul.music.utils.Playlists;
  * @author SkytAsul
  */
 public class JukeBoxInventory implements Listener{
-	
+
 	private static final Pattern NEWLINE_REGEX = Pattern.compile("\\\\n|\\n");
-	
+
 	private static ItemStack stopItem = item(Material.BARRIER, Lang.STOP);
 	private static ItemStack menuItem = item(Material.TRAPPED_CHEST, Lang.MENU_ITEM);
 	private static ItemStack toggleItem = item(JukeBox.version < 9 ? Material.STONE_BUTTON : Material.valueOf("END_CRYSTAL"), Lang.TOGGLE_PLAYING);
@@ -50,16 +43,16 @@ public class JukeBoxInventory implements Listener{
 	private static Material sign = JukeBox.version < 14 ? Material.valueOf("SIGN") : Material.valueOf("OAK_SIGN");
 	private static Material lead = JukeBox.version < 13 ? Material.valueOf("LEASH") : Material.valueOf("LEAD");
 	private static List<String> playlistLore = Arrays.asList("", Lang.IN_PLAYLIST);
-	
+
 	private Material[] discs;
 	private UUID id;
 	public PlayerData pdata;
-	
+
 	private int page = 0;
 	private ItemsMenu menu = ItemsMenu.DEFAULT;
-	
+
 	private Inventory inv;
-	
+
 	public JukeBoxInventory(Player p, PlayerData pdata) {
 		Bukkit.getPluginManager().registerEvents(this, JukeBox.getInstance());
 		this.id = p.getUniqueId();
@@ -71,24 +64,24 @@ public class JukeBoxInventory implements Listener{
 		for (int i = 0; i < discs.length; i++){
 			discs[i] = JukeBox.songItems.get(ran.nextInt(JukeBox.songItems.size()));
 		}
-		
+
 		this.inv = Bukkit.createInventory(null, 54, Lang.INV_NAME);
-		
+
 		setSongsPage(p);
-		
+
 		openInventory(p);
 	}
-	
+
 	public void openInventory(Player p) {
 		inv = p.openInventory(inv).getTopInventory();
 		menu = ItemsMenu.DEFAULT;
 		setItemsMenu();
 	}
-	
+
 	public void setSongsPage(Player p) {
 		inv.setItem(52, item(Material.ARROW, Lang.LATER_PAGE, String.format(Lang.CURRENT_PAGE, page + 1, Math.max(JukeBox.maxPage, 1)))); // max to avoid 0 pages if no songs
 		inv.setItem(53, item(Material.ARROW, Lang.NEXT_PAGE, String.format(Lang.CURRENT_PAGE, page + 1, Math.max(JukeBox.maxPage, 1))));
-		
+
 		for (int i = 0; i < 45; i++) inv.setItem(i, null);
 		if (pdata.getPlaylistType() == Playlists.RADIO) return;
 		if (JukeBox.getSongs().isEmpty()) return;
@@ -101,11 +94,11 @@ public class JukeBoxInventory implements Listener{
 			if (JukeBox.getSongs().size() - 1 == (page*45) + i) break;
 		}
 	}
-	
+
 	public void setItemsMenu() {
 		for (int i = 45; i < 52; i++) inv.setItem(i, null);
 		if (menu != ItemsMenu.DEFAULT) inv.setItem(45, menuItem);
-		
+
 		switch (menu) {
 		case DEFAULT:
 			inv.setItem(45, stopItem);
@@ -135,11 +128,11 @@ public class JukeBoxInventory implements Listener{
 			break;
 		}
 	}
-	
+
 	public UUID getID(){
 		return id;
 	}
-	
+
 	@EventHandler
 	public void onClick(InventoryClickEvent e){
 		Player p = (Player) e.getWhoClicked();
@@ -148,7 +141,7 @@ public class JukeBoxInventory implements Listener{
 		if (!p.getUniqueId().equals(id)) return;
 		e.setCancelled(true);
 		int slot = e.getSlot();
-		
+
 		Material type = e.getCurrentItem().getType();
 		if (JukeBox.songItems.contains(type)) {
 			Song s = JukeBox.getSongs().get(page * 45 + slot);
@@ -162,9 +155,9 @@ public class JukeBoxInventory implements Listener{
 			}else if (pdata.playSong(s)) inv.setItem(slot, loreAdd(getSongItem(s, p), playlistLore));
 			return;
 		}
-		 
+
 		switch (slot){
-			
+
 		case 52:
 		case 53:
 			if (JukeBox.maxPage == 0) break;
@@ -177,7 +170,7 @@ public class JukeBoxInventory implements Listener{
 			}
 			setSongsPage(p);
 			break;
-			
+
 		default:
 			if (slot == 45) {
 				if (menu == ItemsMenu.DEFAULT) {
@@ -189,32 +182,32 @@ public class JukeBoxInventory implements Listener{
 				}
 				return;
 			}
-			
+
 			switch (menu) {
 			case DEFAULT:
 				switch (slot) {
 				case 46:
 					pdata.togglePlaying();
 					break;
-					
+
 				case 47:
 					pdata.playRandom();
 					break;
-				
+
 				case 49:
 					menu = ItemsMenu.PLAYLIST;
 					setItemsMenu();
 					break;
-					
+
 				case 50:
 					menu = ItemsMenu.OPTIONS;
 					setItemsMenu();
 					break;
-					
+
 				}
 				break;
-				
-				
+
+
 			case OPTIONS:
 				switch (slot) {
 				case 47:
@@ -223,97 +216,97 @@ public class JukeBoxInventory implements Listener{
 					if (pdata.getVolume() < 0) pdata.setVolume((byte) 0);
 					if (pdata.getVolume() > 100) pdata.setVolume((byte) 100);
 					break;
-					
+
 				case 48:
 					pdata.setParticles(!pdata.hasParticles());
 					break;
-					
+
 				case 49:
 					if (!JukeBox.autoJoin) pdata.setJoinMusic(!pdata.hasJoinMusic());
 					break;
-					
+
 				case 50:
 					pdata.setShuffle(!pdata.isShuffle());
 					break;
-					
+
 				case 51:
 					pdata.setRepeat(!pdata.isRepeatEnabled());
 					break;
 				}
 				break;
-				
-				
+
+
 			case PLAYLIST:
 				switch (slot) {
 				case 47:
 					pdata.nextSong();
 					break;
-				
+
 				case 48:
 					pdata.clearPlaylist();
 					setSongsPage(p);
 					break;
-					
+
 				case 50:
 					pdata.nextPlaylist();
 					setSongsPage(p);
 					break;
-				
+
 				}
 				break;
 			}
 			break;
-		
+
 		}
 	}
-	
+
 	public ItemStack getSongItem(Song s, Player p) {
 		ItemStack is = item(discs[JukeBox.getSongs().indexOf(s)], JukeBox.getItemName(s, p));
 		if (s.getDescription() != null && !s.getDescription().isEmpty()) loreAdd(is, splitOnSpace(JukeBox.format(JukeBox.descriptionFormat, JukeBox.descriptionFormatWithoutAuthor, s), 30));
 		return is;
 	}
-	
+
 	public void volumeItem(){
 		if (menu == ItemsMenu.OPTIONS) name(inv.getItem(47), Lang.VOLUME + pdata.getVolume() + "%");
 	}
-	
+
 	public void particlesItem(){
 		if (menu != ItemsMenu.OPTIONS) return;
 		if (!JukeBox.particles) return;
 		if (!JukeBox.particles) inv.setItem(48, null);
 		name(inv.getItem(48), ChatColor.AQUA + replaceToggle(Lang.TOGGLE_PARTICLES, pdata.hasParticles()));
 	}
-	
+
 	public void joinItem(){
 		if (menu == ItemsMenu.OPTIONS) name(inv.getItem(49), ChatColor.GREEN + replaceToggle(Lang.TOGGLE_CONNEXION_MUSIC, pdata.hasJoinMusic()));
 	}
-	
+
 	public void shuffleItem(){
 		if (menu == ItemsMenu.OPTIONS) name(inv.getItem(50), ChatColor.YELLOW + replaceToggle(Lang.TOGGLE_SHUFFLE_MODE, pdata.isShuffle()));
 	}
-	
+
 	public void repeatItem(){
 		if (menu == ItemsMenu.OPTIONS) name(inv.getItem(51), ChatColor.GOLD + replaceToggle(Lang.TOGGLE_LOOP_MODE, pdata.isRepeatEnabled()));
 	}
-	
+
 	private String replaceToggle(String string, boolean enabled) {
 		return string.replace("{TOGGLE}", enabled ? Lang.DISABLE : Lang.ENABLE);
 	}
-	
+
 	public void playingStarted() {
 		if (menu == ItemsMenu.DEFAULT) inv.setItem(46, toggleItem);
 	}
-	
+
 	public void playingStopped() {
 		if (menu == ItemsMenu.DEFAULT) inv.setItem(46, null);
 	}
-	
+
 	public void playlistItem(){
 		if (menu == ItemsMenu.PLAYLIST)
 			inv.setItem(50, pdata.getPlaylistType().item);
 		else if (menu == ItemsMenu.OPTIONS) setItemsMenu();
 	}
-	
+
 	public void songItem(int id, Player p) {
 		if (!(id > page*45 && id < (page+1)*45) || pdata.getPlaylistType() == Playlists.RADIO) return;
 		Song song = JukeBox.getSongs().get(id);
@@ -321,9 +314,9 @@ public class JukeBoxInventory implements Listener{
 		if (pdata.isInPlaylist(song)) loreAdd(is, playlistLore);
 		inv.setItem(id - page*45, is);
 	}
-	
 
-	
+
+
 
 	public static ItemStack item(Material type, String name, String... lore) {
 		ItemStack is = new ItemStack(type);
@@ -334,7 +327,7 @@ public class JukeBoxInventory implements Listener{
 		is.setItemMeta(im);
 		return is;
 	}
-	
+
 	public static ItemStack loreAdd(ItemStack is, List<String> lore){
 		ItemMeta im = is.getItemMeta();
 		List<String> ls = im.getLore();
@@ -355,7 +348,7 @@ public class JukeBoxInventory implements Listener{
 
 	public static final ItemStack radioItem;
     static {
-        GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+		GameProfile profile = new GameProfile(UUID.randomUUID(), "randomName");
         PropertyMap propertyMap = profile.getProperties();
         propertyMap.put("textures", new Property("textures", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMTQ4YThjNTU4OTFkZWM3Njc2NDQ0OWY1N2JhNjc3YmUzZWU4OGEwNjkyMWNhOTNiNmNjN2M5NjExYTdhZiJ9fX0="));
         ItemStack item;
@@ -378,15 +371,15 @@ public class JukeBoxInventory implements Listener{
         item.setItemMeta(headMeta);
         radioItem = item;
     }
-	
+
 	public static List<String> splitOnSpace(String string, int minSize){
-		if (string == null || string.isEmpty()) return null; 
+		if (string == null || string.isEmpty()) return null;
 		List<String> ls = new ArrayList<>();
 		if (string.length() <= minSize){
 			ls.add(string);
 			return ls;
 		}
-		
+
 		for (String str : NEWLINE_REGEX.split(string)) {
 			int lastI = 0;
 			int ic = 0;
@@ -408,12 +401,12 @@ public class JukeBoxInventory implements Listener{
 				ic++;
 			}
 		}
-		
+
 		return ls;
 	}
-	
+
 	enum ItemsMenu{
 		DEFAULT, OPTIONS, PLAYLIST;
 	}
-	
+
 }
